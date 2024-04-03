a65e123c870085c9238396a3a942baee55911c47
vitor paiva <paivavitorr@gmail.com>
2024-04-03 15:43:40 -0400

    aeee

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { StarIcon, FilmIcon } from "@heroicons/react/24/outline";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -66,11 +67,28 @@ function App() {
   };
 
   return (
-    <div className="bg-slate-800 text-white min-h-screen py-8">
-      <div className="container mx-auto flex justify-center items-center">
-        <div className='flex-1'>
-          <div className="MovieRating">
-            <h1 className="text-3xl mb-7">FeedCine</h1>
+    <div className="max-w-5xl  mx-auto bg-slate-800 text-white min-h-screen py-8">
+      <div className=" flex flex-col gap-5 ">
+        {}
+        <nav className="flex justify-between items-center pb-5 shadow-2xl px-10">
+          <div className="flex items-center">
+            <FilmIcon className='h-10 w'/>
+            <h1 className="text-3xl ml-2"> FeedCine</h1>
+          </div>
+          <div className="flex items-center">
+            <input
+              type="text"
+              placeholder="Pesquisar..."
+              className="px-4 py-2 mr-4 rounded border-2 border-gray-500 bg-transparent text-white placeholder-gray-500"
+            />
+            <a href="#" className="mr-4 hover:text-gray-300">Início</a>
+            <a href="#" className="hover:text-gray-300">Quem Somos</a>
+          </div>
+        </nav>
+
+        {/* Conteúdo da aplicação */}
+        <div className="flex justify-center">
+          <div className="MovieRating w-full max-w-lg">
             <input
               type="text"
               placeholder="Título do Filme"
@@ -83,12 +101,12 @@ function App() {
               onChange={handleRatingChange}
               className="px-4 py-2 w-full rounded border-2 border-gray-500 bg-transparent text-white mb-4"
             >
-              <option value="0" className="text-black">Selecione uma classificação</option>
-              <option value="1" className="text-black">1 estrela</option>
-              <option value="2" className="text-black">2 estrelas</option>
-              <option value="3" className="text-black">3 estrelas</option>
-              <option value="4" className="text-black">4 estrelas</option>
-              <option value="5" className="text-black">5 estrelas</option>
+              <option value="0" className="text-black">⭐ Selecione uma classificação </option>
+              <option value="1" className="text-black">⭐ 1 estrela</option>
+              <option value="2" className="text-black">⭐ 2 estrelas</option>
+              <option value="3" className="text-black">⭐ 3 estrelas</option>
+              <option value="4" className="text-black">⭐ 4 estrelas</option>
+              <option value="5" className="text-black">⭐ 5 estrelas</option>
             </select>
             <textarea
               placeholder="Seu comentário"
@@ -113,25 +131,24 @@ function App() {
               <div className="mt-4">
                 <h2 className="text-xl mb-2">Filmes Avaliados:</h2>
                 <ul>
-                {movies.map((movie, index) => (
-  <li key={index} className="flex items-center">
-    <div className="mr-4">
-      <strong>{movie.title}</strong> - {movie.category}
-      <br />
-      <img src="" alt={movie.title} className="mt-1 h-15" />
-    </div>
-    <div className='stari'>
-      {movie.rating} estrelas
-      <div className="comment-box bg-white p-2 rounded-md shadow-md mt-2 inline-block min-w-min">
-        <div className="text-black">
-          <span className="text-sm">{movie.comment}</span>
-        </div>
-      </div>
-      <button onClick={() => handleRemoveMovie(index)} className="px-2 py-1 ml-2 bg-red-500 text-white rounded hover:bg-red-600">Remover</button>
-    </div>
-  </li>
-))}
-
+                  {movies.map((movie, index) => (
+                    <li key={index} className="flex items-center">
+                      <div className="mr-4 flex gap-2">
+                        <strong>{movie.title}</strong>
+                        <img src={movie.image} alt={movie.title} style={{height: "50px", width: "auto"}} />
+                      </div>
+                      <div className="flex items-center">
+                        <StarIcon className="h-5 w-5 text-yellow-400 mr-1" />
+                        <span>{movie.rating} estrelas</span>
+                      </div>
+                      <div className="bg-white p-2 rounded-md m-4 inline-block min-w-min">
+                        <div className="text-black">
+                          <span className="text-sm">{movie.comment}</span>
+                        </div>
+                      </div>
+                      <button onClick={() => handleRemoveMovie(index)} className="px-2 py-1 ml-2 bg-red-500 text-white rounded hover:bg-red-600">Remover</button>
+                    </li>
+                  ))}
                 </ul>
               </div>
             )}
