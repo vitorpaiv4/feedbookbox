c1cb7c5a1c69c5285cd47c795f795d9ac2089a33
vitor paiva <paivavitorr@gmail.com>
2024-04-03 10:23:03 -0400

    Boaa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import MovieRating from './components/MovieRating';
-import BookRating from './components/BookRating';
 
 function App() {
-  const [books, setBooks] = useState([]);
   const [movies, setMovies] = useState([]);
   const [title, setTitle] = useState('');
   const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
+  const [category, setCategory] = useState('Ação e Aventura');
 
   useEffect(() => {
     loadList();
@@ -14,14 +13,9 @@ function App() {
 
   useEffect(() => {
     saveList();
-  }, [books, movies]);
+  }, [movies]);
 
   const loadList = () => {
-    const savedBooks = localStorage.getItem('books');
-    if (savedBooks) {
-      setBooks(JSON.parse(savedBooks));
-    }
-
     const savedMovies = localStorage.getItem('movies');
     if (savedMovies) {
       setMovies(JSON.parse(savedMovies));
@@ -29,7 +23,6 @@ function App() {
   };
 
   const saveList = () => {
-    localStorage.setItem('books', JSON.stringify(books));
     localStorage.setItem('movies', JSON.stringify(movies));
   };
 
@@ -41,28 +34,104 @@ function App() {
     setRating(parseInt(event.target.value));
   };
 
-  const handleAddBook = () => {
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleAddMovie = () => {
     if (title.trim() === '' || rating < 1 || rating > 5) return;
 
-    const newBook = {
+    const newMovie = {
       title: title,
-      rating: rating
+      rating: rating,
+      comment: comment,
+      category: category
     };
 
-    setBooks([...books, newBook]);
+    setMovies([...movies, newMovie]);
     setTitle('');
     setRating(0);
+    setComment('');
+    setCategory('Ação e Aventura');
+  };
+
+  const handleRemoveMovie = (index) => {
+    const updatedMovies = [...movies];
+    updatedMovies.splice(index, 1);
+    setMovies(updatedMovies);
   };
 
   return (
     <div className="bg-slate-800 text-white min-h-screen py-8">
       <div className="container mx-auto flex justify-center items-center">
         <div className='flex-1'>
-          <BookRating />
-        </div>
-        <div className='flex-1'>
-          <div className="MovieRating flex flex-col justify-start">
-            <MovieRating movies={movies} setMovies={setMovies} />
+          <div className="MovieRating">
+            <h1 className="text-3xl mb-4">Avaliador de Filmes</h1>
+            <input
+              type="text"
+              placeholder="Título do Filme"
+              value={title}
+              onChange={handleTitleChange}
+              className="px-4 py-2 w-full rounded border-2 border-gray-500 bg-transparent text-white placeholder-gray-500 mb-4"
+            />
+            <select
+              value={rating}
+              onChange={handleRatingChange}
+              className="px-4 py-2 w-full rounded border-2 border-gray-500 bg-transparent text-white mb-4"
+            >
+              <option value="0" className="text-black">Selecione uma classificação</option>
+              <option value="1" className="text-black">1 estrela</option>
+              <option value="2" className="text-black">2 estrelas</option>
+              <option value="3" className="text-black">3 estrelas</option>
+              <option value="4" className="text-black">4 estrelas</option>
+              <option value="5" className="text-black">5 estrelas</option>
+            </select>
+            <textarea
+              placeholder="Seu comentário"
+              value={comment}
+              onChange={handleCommentChange}
+              className="px-4 py-2 w-full rounded border-2 border-gray-500 bg-transparent text-white mb-4"
+            ></textarea>
+            <select
+              value={category}
+              onChange={handleCategoryChange}
+              className="px-4 py-2 w-full rounded border-2 border-gray-500 bg-transparent text-white mb-4"
+            >
+              <option value="Ação e Aventura" className="text-black">Ação e Aventura</option>
+              <option value="Drama" className="text-black">Drama</option>
+              <option value="Comédia romântica" className="text-black">Comédia romântica</option>
+              <option value="Ficção científica" className="text-black">Ficção científica</option>
+              <option value="Terror" className="text-black">Terror</option>
+            </select>
+            <button onClick={handleAddMovie} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Adicionar Filme</button>
+            {movies.length > 0 && (
+              <div className="mt-4">
+                <h2 className="text-xl mb-2">Filmes Avaliados:</h2>
+                <ul>
+                  {movies.map((movie, index) => (
+                    <li key={index} className="flex items-center">
+                      <div className="mr-4">
+                        <strong>{movie.title}</strong>
+                        <br />
+                        <img src="" alt={movie.title} className="mt-1 h-24" />
+                      </div>
+                      <div>
+                        Categoria: {movie.category}, Estrelas: {movie.rating}
+                        <br />
+                        <div className="comment-box bg-white p-2 rounded-md shadow-md mt-2 inline-block min-w-min">
+                          <div className='text-black'> <span className='text-sm'>{movie.comment}</span> </div>
+                        </div>
+                        <button onClick={() => handleRemoveMovie(index)} className="px-2 py-1 ml-2 bg-red-500 text-white rounded hover:bg-red-600">Remover</button>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
